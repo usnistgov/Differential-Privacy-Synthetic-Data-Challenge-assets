326f96f3ce021655c13620a1315bdf4d6fff171c
Gary Howarth <54448497+garyhowarth@users.noreply.github.com>
2021-11-09 10:39:09 -0700

    Separating 2020 and 2019 DP challenges into two repos

--- 2020 DP Temporal Map Challenge - Solutions and Data/competition-runtime/sprint-1/runtime/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running Python tests"
-conda run -n py-$1 python tests/test_installs.py
-
-echo "Running R tests"
-conda run -n r-$1 Rscript tests/test_installs.R
--- 2020 DP Temporal Map Challenge - Solutions and Data/competition-runtime/sprint-3/runtime/conftest.py
+++
@@ -1,2 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--submission-path", action="store", default="submission.csv")
